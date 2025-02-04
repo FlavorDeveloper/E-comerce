4b26b92a8c5d4e1dd8cb82e20db5942440a660f2
Flavor Developer <161358985+FlavorDeveloper@users.noreply.github.com>
2025-02-04 19:00:54 -0400

    Add files via upload

---
+++ addBtn.js
@@ -0,0 +1,25 @@
+export function addBtn () {
+// Selectores y variables.
+    const addCartBtn  = document.querySelectorAll('.add-to-cart');
+    const root        = document.documentElement;
+    const textsColor  = getComputedStyle(root).getPropertyValue('--texts-color').trim();
+    const textsColor2 = getComputedStyle(root).getPropertyValue('--background-color').trim();
+    const textos      = document.querySelector('.add-to-cart').textContent;
+    const addBtnColor = getComputedStyle(root).getPropertyValue('--addbtn-color');
+
+// Eventos.
+    addCartBtn.forEach(boton => {
+        boton.addEventListener('click', (e) => {
+            e.stopPropagation();
+            boton.style.backgroundColor = addBtnColor;
+            boton.style.color = textsColor2;
+            boton.textContent = 'Añadido ✔';
+
+            setTimeout(() => {
+                boton.style.backgroundColor = ""; 
+                boton.style.color = ""; 
+                boton.textContent = textos;
+            }, 2000);
+        });
+    });
+};
