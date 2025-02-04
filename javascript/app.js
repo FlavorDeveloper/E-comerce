70a5e4e39a28f17a9841754f5061d465877b0f44
Flavor Developer <161358985+FlavorDeveloper@users.noreply.github.com>
2025-02-04 10:53:33 -0400

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+// En app.js
+import { navBar } from "./modules/navBar.js";
+import { showCarrito } from "./modules/showCarrito.js";
+import { zoomImg } from './modules/zoomImg.js';
+
+navBar();
+showCarrito();
+zoomImg();
+
+
