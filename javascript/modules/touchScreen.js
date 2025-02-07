f927a7f48c7db30bb496dd4310c8aea3f16fe182
Flavor Developer <161358985+FlavorDeveloper@users.noreply.github.com>
2025-02-07 13:23:35 -0400

    Delete javascript/modules/touchScreen.js

--- javascript/modules/touchScreen.js
+++
@@ -1,6 +0,0 @@
-export function touchScreen () {
-    document.addEventListener('gestureend', () => {
-        document.body.style.transform = 'scale(1)';
-        document.body.style.transformOrigin = 'center';
-    });
-};
